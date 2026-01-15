028ab942f4afb62c4aca261e24f0fae7e3f2be63
Evan Khosh <evank43@nycstudents.net>
2026-01-15 11:33:04 -0500

    Added error for change_username when the new username is empty

--- app/data.py
+++ app/data.py
@@ -83,8 +83,8 @@ def register_user(username, password):
     password = password.encode('utf-8')
     password = str(hashlib.sha256(password).hexdigest())
 
-    command = 'INSERT INTO user_info VALUES (?, ?, NULL, NULL)'
-    vars = (username, password)
+    command = 'INSERT INTO user_info VALUES (?, ?, NULL, ?)'
+    vars = (username, password, 0)
     c.execute(command, vars)
 
     db.commit()
@@ -94,6 +94,9 @@ def register_user(username, password):
 
 
 def change_username(old_username, new_username):
+
+    if new_username == "":
+        raise ValueError("Your new username must be non-empty")
 
     if user_exists(new_username):
         raise ValueError("Username already exists")
@@ -295,7 +298,7 @@ def new_flashcard(title, flashcard_content, creator):
     db.commit()
     db.close()
 
-    return title 
+    return title
 
 
 #----------FLASHCARDS-HELPERS----------#
